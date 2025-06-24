d5f3bee14ee5130d6e6c3645b98508db0d3b466b
Kishan Kumar Vadhia <kishan.vadhia@zeuslearning.com>
2025-06-24 19:29:23 +0530

    Rows

---
+++ build/js/app.js
@@ -0,0 +1,11 @@
+import { RowsCanvas } from './main.js';
+const obj = new RowsCanvas(0, { [20]: { height: 500 } }, 80, 24);
+const div = obj.createRowCanvas(0);
+const obj2 = new RowsCanvas(1, {}, 80, 24);
+const div2 = obj2.createRowCanvas(1);
+const obj3 = new RowsCanvas(2, {}, 80, 24);
+const div3 = obj3.createRowCanvas(2);
+const temp = document.querySelector('.rowsColumn');
+temp === null || temp === void 0 ? void 0 : temp.appendChild(div);
+temp === null || temp === void 0 ? void 0 : temp.appendChild(div2);
+temp === null || temp === void 0 ? void 0 : temp.appendChild(div3);
